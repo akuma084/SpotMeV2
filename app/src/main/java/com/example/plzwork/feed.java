b913f5d2f9379093bebc033caaee97b3975e3718
akuma084 <ak057024@gmail.com>
2022-04-20 15:31:00 -0700

    added Ui elements to feeed

--- app/src/main/java/com/example/plzwork/feed.java
+++ app/src/main/java/com/example/plzwork/feed.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class feed extends AppCompatActivity {
+    FirebaseDatabase user_database = FirebaseDatabase.getInstance();
+    DatabaseReference mDb = user_database.getReference();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final DatabaseReference root = user_database.getReference().child("Posts");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
