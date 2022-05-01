90746d1d57bec61949502100d257232cfec95dc2
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-05-01 16:58:45 -0700

    update write to database

--- app/src/main/java/com/example/plzwork/AddPost.java
+++ app/src/main/java/com/example/plzwork/AddPost.java
@@ -7,15 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class AddPost extends AppCompatActivity {
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private final DatabaseReference root = database.getReference().child("Posts");
+    private final DatabaseReference root = database.getReference().child("Feed");
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();//get instance of the FirebaseAuth object
+    FirebaseUser user = mAuth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,15 @@ public class AddPost extends AppCompatActivity {
 
 
     public void onPostButtonClicked(View view) {
-        //HashMap<String, Object> usermap = new HashMap<>();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+        String year = cal.get(Calendar.YEAR) * -1 + "";
+        String month = cal.get(Calendar.MONTH) * -1 + "";
+        String day = cal.get(Calendar.DAY_OF_MONTH) * -1 + "";
         EditText text = (EditText)findViewById(R.id.postDescription);
-//        String message = text.getText().toString();
-//        root.setValue(message);
-
+        String contents = text.getText().toString();
+        if(contents != "") {
+            root.child(year).child(month).child(day).child(user.getUid()).setValue(contents);
+        }
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
